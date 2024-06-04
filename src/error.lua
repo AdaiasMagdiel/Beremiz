dab587ed3c5ece6a51531f6346ca1cca2c4fc73b
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-06-04 10:38:59 -0300

    feat: add underlining of error in the output
    - Improved the error display by adding underlining to the code snippet, highlighting the exact location of the error.
    - Changed the color of the error line number.
    - Increased the spacing between the code and error information to improve readability.

--- src/error.lua
+++ src/error.lua
@@ -1,6 +1,24 @@
 local pprint = require("lib.pprint")
 local Error = {}
 
+local function getNextSpace(actualCol, line)
+	local col = actualCol+1
+
+	while true do
+		if col > #line then
+			break
+		end
+
+		if line:sub(col,  col) == " " then
+			break
+		end
+
+		col = col+1
+	end
+
+	return col
+end
+
 function Error.splitLines(raw)
 	local lines = {}
 
@@ -13,20 +31,25 @@ end
 
 function Error.show(message, token, lines)
 	io.write(
-		("%s:%d:%d | %s\n\n")
+		("\x1b[31m%s:%d:%d\x1b[0m | %s\n\n")
 		:format(token.loc.file, token.loc.line, token.loc.col, message)
 	)
 
 	local line = lines[token.loc.line]
-	local space = "."
+	local space = " "
 	local lineNum = token.loc.line
 	local lineCol = token.loc.col
-	local errorInfo = ("%d:%d | "):format(lineNum, lineCol)
+	local errorInfo = ("\x1b[31m%d:%d\x1b[0m | "):format(lineNum, lineCol)
+
+	local offset = 5
+
+	local nextSpaceCol = getNextSpace(lineCol, line)
+	local under = ("~"):rep(nextSpaceCol - lineCol)
 
 	io.write(("%s%s\n"):format(errorInfo, line))
 	io.write(
-		(space):rep(#errorInfo + lineCol),
-		"^",
+		space:rep(lineCol-1 + offset),
+		("\x1b[31m^%s\x1b[0m"):format(under),
 		'\n'
 	)
 
