a3cb409f0a5c3040d34439479e2c40c3adeffea6
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-06-07 20:33:49 -0300

    feat: Add string.split method
    - The `string.split` method takes two arguments: the string to split and the separator.
    - The method returns a list of strings, with each string representing a part of the original string that was separated by the separator.
    
    The `string.split` method uses the `gmatch` function to iterate over the string and split it into parts based on the separator. Each part is then added to a list, which is returned by the method.

--- src/modules/string.lua
+++ src/modules/string.lua
@@ -2,35 +2,44 @@ local string_methods = {}
 
 local Error = require("src.error")
 local utils = require("src.utils")
+local pprint = require("pprint")
 
 local Tokens = require("src.token")
 local Token = Tokens.Token
 local TokenType = Tokens.TokenType
 
-local function validateCount(stack, count, method_token, lines)
-	if #stack < count then
-		Error.error(
+local ErrorInfo = {
+	stack = {},
+	lines = {},
+	method_token = {}
+}
+
+local function validateCount(count)
+	if #ErrorInfo.stack < count then
+		Error.show(
 			("Expected %d arguments for 'string.%s', but only found %d arguments on the stack."):
-			format(count, method_token.value, #stack),
-			method_token,
-			lines
+			format(count, ErrorInfo.method_token.value, #ErrorInfo.stack),
+			ErrorInfo.method_token,
+			ErrorInfo.lines
 		)
 	end
 end
 
-local function validateType(stack, token, method_token, lines)
-	if token.type ~= TokenType.STRING then
-		Error.error(
+local function validateType(token, type_)
+	type_ = type_ or TokenType.STRING
+
+	if token.type ~= type_ then
+		Error.show(
 			("Expected string type for 'string.%s' but encountered '%s'."):
-			format(method_token.value, token.type:lower()),
-			method_token,
-			lines
+			format(ErrorInfo.method_token.value, token.type:lower()),
+			token,
+			ErrorInfo.lines
 		)
 	end
 end
 
-local function applyMethod(stack, token, method)
-	utils.push(stack, Token.new(
+local function applyMethod(token, method)
+	utils.push(ErrorInfo.stack, Token.new(
 		TokenType.STRING,
 		method(token.value),
 		token.loc
@@ -38,23 +47,55 @@ local function applyMethod(stack, token, method)
 end
 
 function string_methods.upper(stack, method_token, lines)
-	validateCount(stack, 1, method_token, lines)
+	ErrorInfo.stack = stack
+	ErrorInfo.lines = lines
+	ErrorInfo.method_token = method_token
 
-	local token = utils.pop(stack)
+	validateCount(1)
+	local token = utils.pop(ErrorInfo.stack)
+	validateType(token)
 
-	validateType(stack, token, method_token, lines)
-
-	applyMethod(stack, token, string.upper)
+	applyMethod(token, string.upper)
 end
 
 function string_methods.lower(stack, method_token, lines)
-	validateCount(stack, 1, method_token, lines)
+	ErrorInfo.stack = stack
+	ErrorInfo.lines = lines
+	ErrorInfo.method_token = method_token
+
+	validateCount(1)
+	local token = utils.pop(ErrorInfo.stack)
+	validateType(token)
+
+	applyMethod(token, string.lower)
+end
+
+function string_methods.split(stack, method_token, lines)
+	ErrorInfo.stack = stack
+	ErrorInfo.lines = lines
+	ErrorInfo.method_token = method_token
+
+	validateCount(2)
+
+	local separator = utils.pop(stack)
+	validateType(separator)
 
 	local token = utils.pop(stack)
+	validateType(token)
 
-	validateType(stack, token, method_token, lines)
+	local parts = {}
 
-	applyMethod(stack, token, string.lower)
+	for part in token.value:gmatch(("([^%s]+)"):format(separator.value)) do
+		table.insert(parts, 1, part)
+	end
+
+	for _, part in ipairs(parts) do
+		utils.push(stack, Token.new(
+			TokenType.STRING,
+			part,
+			token.loc
+		))
+	end
 end
 
 return string_methods
