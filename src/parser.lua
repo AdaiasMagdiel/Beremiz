8e0c2bd57491a2693d0e63686512a85e1ad652e3
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-06-07 19:12:16 -0300

    feat: Refactor code using utility functions and improve error handling
    - **Refactored code using utility functions**: The `push`, `pop`, and `include` functions have been extracted into a dedicated `utils` module for better organization and reusability.
    - **Improved error handling**: Enhanced error messages provide more specific information about the problem encountered, including line numbers and context.
    - **Added `ACCESS` functionality**: The `ACCESS` keyword allows calling methods of modules, enabling more complex operations.

--- src/parser.lua
+++ src/parser.lua
@@ -1,27 +1,23 @@
 local pprint = require("lib.pprint")
 
+local utils = require("src.utils")
 local Lexer = require("src.lexer")
 local Error = require("src.error")
+
 local Tokens = require("src.token")
+local Token = Tokens.Token
 local TokenType = Tokens.TokenType
 
 local Parser = {
 	program = {},
 	stack = {},
 	lines = {},
-	defines = {}
+	defines = {},
+	env = {}
 }
 
 Parser.__index = Parser
 Parser.debug = true
-
-local function push(stack, value)
-	stack[#stack + 1] = value
-end
-
-local function pop(stack)
-	return table.remove(stack, #stack)
-end
 
 local function readFile(filepath)
 	-- First: Verify in user local folder
@@ -51,8 +47,11 @@ function Parser:new(tokens, raw)
 
 	tokens = obj:include(tokens)
 	tokens = obj:crossref(tokens)
-
 	obj.program = tokens
+
+	obj.env = {
+		["string"] = require("modules.string")
+	}
 
 	return obj
 end
@@ -125,9 +124,29 @@ function Parser:crossref(tokens)
 		if token.type == TokenType.DEFINE then
 			local tokenName = tokens[ip+1]
 
+			if tokenName == nil then
+				Error.show(
+					"Syntax Error: Expected 'identifier'.",
+					token,
+					self.lines
+				)
+				return
+			end
+
 			if tokenName.type ~= TokenType.IDENTIFIER then
 				Error.show(
-					("Syntax Error: Expected 'identifier' but encountered '%s'."):format(tokenName.type:lower()),
+					("Syntax Error: Expected 'identifier' but encountered '%s'."):
+					format(tokenName.type:lower()),
+					tokenName,
+					self.lines
+				)
+			end
+
+			if self.env[tokenName.value] ~= nil then
+				Error.show(
+					"You are trying to redefine the reserved keyword '%s'. " ..
+					("Reserved keywords are used by the language and cannot be changed."):
+					format(tokenName.value),
 					tokenName,
 					self.lines
 				)
@@ -135,13 +154,13 @@ function Parser:crossref(tokens)
 
 			self.defines[tokenName.value] = ip+2 -- after define name
 
-			push(ip_stack, ip)
+			utils.push(ip_stack, ip)
 
 		elseif token.type == TokenType.IF then
-			push(ip_stack, ip)
+			utils.push(ip_stack, ip)
 
 		elseif token.type == TokenType.ELSE then
-			local ip_if = pop(ip_stack)
+			local ip_if = utils.pop(ip_stack)
 
 			if ip_if == nil or tokens[ip_if].type ~= TokenType.IF then
 				Error.show(
@@ -153,16 +172,16 @@ function Parser:crossref(tokens)
 
 			tokens[ip_if].jump_ip = ip+1
 
-			push(ip_stack, ip)
+			utils.push(ip_stack, ip)
 
 		elseif token.type == TokenType.WHILE then
-			push(ip_stack, ip)
+			utils.push(ip_stack, ip)
 
 		elseif token.type == TokenType.DO then
-			push(ip_stack, ip)
+			utils.push(ip_stack, ip)
 
 		elseif token.type == TokenType.END then
-			local ip_block = pop(ip_stack)
+			local ip_block = utils.pop(ip_stack)
 
 			if ip_block == nil or
 			   tokens[ip_block].type ~= TokenType.IF     and
@@ -178,7 +197,7 @@ function Parser:crossref(tokens)
 			end
 
 			if tokens[ip_block].type == TokenType.DO then
-				local ip_while = pop(ip_stack)
+				local ip_while = utils.pop(ip_stack)
 
 				if ip_while == nil or tokens[ip_while].type ~= TokenType.WHILE then
 					Error.show(
@@ -210,11 +229,15 @@ function Parser:parse()
 		end
 
 		if token.type == TokenType.NUMBER then
-			push(self.stack, token.value)
+			utils.push(self.stack, token)
 			ip = ip + 1
 
 		elseif token.type == TokenType.NIL then
-			push(self.stack, token.value)
+			utils.push(self.stack, token)
+			ip = ip + 1
+
+		elseif token.type == TokenType.BOOL then
+			utils.push(self.stack, token)
 			ip = ip + 1
 
 		elseif token.type == TokenType.STRING then
@@ -222,7 +245,7 @@ function Parser:parse()
 			local pattern = "[^\\]?%$(%d+)"   -- Match $1, $42, $890, ... with escape \$
 
 			if not token.value:find(pattern) then
-				push(self.stack, token.value)
+				utils.push(self.stack, token)
 
 			else
 				local delims = {}
@@ -246,7 +269,7 @@ function Parser:parse()
 
 				local values = {}
 				for _ = 1,max+1 do
-					values[#values+1] = pop(self.stack)
+					values[#values+1] = utils.pop(self.stack)
 				end
 
 				local newString = token.value:gsub(pattern,
@@ -254,7 +277,11 @@ function Parser:parse()
 					return " " .. tostring(values[tonumber(str)+1])
 				end)
 
-				push(self.stack, newString)
+				utils.push(self.stack, Token.new(
+					TokenType.STRING,
+					newString,
+					token.loc
+				))
 			end
 
 			ip = ip + 1
@@ -269,101 +296,172 @@ function Parser:parse()
 			token.type == TokenType.EQUAL   or
 			token.type == TokenType.NEQUAL
 		then
-			local b = pop(self.stack)
-			local a = pop(self.stack)
+			if #self.stack < 2 then
+				Error.show(
+					("Expected %d arguments for this operation, but only found %d arguments on the stack."):
+					format(2, #self.stack),
+					token,
+					self.lines
+				)
+			end
 
-			local type_a = type(a)
-			local type_b = type(b)
+			local b = utils.pop(self.stack)
+			local a = utils.pop(self.stack)
 
 			ip = ip + 1
 
 			if token.type == TokenType.PLUS then
-				if type_a == "number" and type_b == "number" then
-					push(self.stack, a + b)
-				elseif type_a == "string" and type_b == "string" then
-					push(self.stack, a .. b)
+				if a.type == TokenType.NUMBER and b.type == TokenType.NUMBER then
+					utils.push(self.stack, Token.new(
+						TokenType.NUMBER,
+						a.value + b.value,
+						token.loc
+					))
+
+				elseif a.type == TokenType.STRING and b.type == TokenType.STRING then
+					utils.push(self.stack, Token.new(
+						TokenType.STRING,
+						a.value .. b.value,
+						token.loc
+					))
+
 				else
 					Error.show(
-				    	("Attempt to add a '%s' with a '%s'."):format(type_a, type_b),
+				    	("Attempt to add a '%s' with a '%s'."):
+				    	format(a.type:lower(), b.type:lower()),
 				    	token,
 				    	self.lines
 				    )
 				end
 
 			elseif token.type == TokenType.MINUS then
-				if type_a == "number" and type_b == "number" then
-					push(self.stack, a - b)
+				if a.type == TokenType.NUMBER and b.type == TokenType.NUMBER then
+					utils.push(self.stack, Token.new(
+						TokenType.NUMBER,
+						a.value - b.value,
+						token.loc
+					))
+
 				else
 					Error.show(
-				    	("Attempt to sub a '%s' with a '%s'."):format(type_a, type_b),
+				    	("Attempt to sub a '%s' with a '%s'."):format(a.type:lower(), b.type:lower()),
 				    	token,
 				    	self.lines
 				    )
 				end
 
 			elseif token.type == TokenType.STAR then
-				if type_a == "number" and type_b == "number" then
-					push(self.stack, a * b)
+				if a.type == TokenType.NUMBER and b.type == TokenType.NUMBER then
+					utils.push(self.stack, Token.new(
+						TokenType.NUMBER,
+						a.value * b.value,
+						token.loc
+					))
+
 				else
 					Error.show(
-				    	("Attempt to mul a '%s' with a '%s'."):format(type_a, type_b),
+				    	("Attempt to mul a '%s' with a '%s'."):format(a.type:lower(), b.type:lower()),
 				    	token,
 				    	self.lines
 				    )
 				end
 
 			elseif token.type == TokenType.SLASH then
-				if type_a == "number" and type_b == "number" then
-					push(self.stack, a / b)
+				if a.type == TokenType.NUMBER and b.type == TokenType.NUMBER then
+					utils.push(self.stack, Token.new(
+						TokenType.NUMBER,
+						a.value / b.value,
+						token.loc
+					))
+
 				else
 					Error.show(
-				    	("Attempt to div a '%s' with a '%s'."):format(type_a, type_b),
+				    	("Attempt to div a '%s' with a '%s'."):format(a.type:lower(), b.type:lower()),
 				    	token,
 				    	self.lines
 				    )
 				end
 
 			elseif token.type == TokenType.GREATER then
-				if type_a == "number" and type_b == "number" then
-					push(self.stack, a > b)
+				if a.type == TokenType.NUMBER and b.type == TokenType.NUMBER then
+					utils.push(self.stack, Token.new(
+						TokenType.BOOL,
+						tostring(a.value > b.value),
+						token.loc
+					))
+
 				else
 					Error.show(
-				    	("Attempt to compare '%s' with '%s'."):format(type_a, type_b),
+				    	("Attempt to compare '%s' with '%s'."):format(a.type:lower(), b.type:lower()),
 				    	token,
 				    	self.lines
 				    )
 				end
 
 			elseif token.type == TokenType.MOD then
-				if type_a == "number" and type_b == "number" then
-					push(self.stack, a % b)
+				if a.type == TokenType.NUMBER and b.type == TokenType.NUMBER then
+					utils.push(self.stack, Token.new(
+						TokenType.NUMBER,
+						a.value % b.value,
+						token.loc
+					))
+
 				else
 					Error.show(
-				    	("Attempt to mod a '%s' with a '%s'."):format(type_a, type_b),
+				    	("Attempt to mod a '%s' with a '%s'."):format(a.type:lower(), b.type:lower()),
 				    	token,
 				    	self.lines
 				    )
 				end
 
 			elseif token.type == TokenType.EQUAL then
-				push(self.stack, a == b)
+				utils.push(self.stack, Token.new(
+					TokenType.BOOL,
+					tostring(a.value == b.value),
+					token.loc
+				))
 
 			elseif token.type == TokenType.NEQUAL then
-				push(self.stack, a ~= b)
+				utils.push(self.stack, Token.new(
+					TokenType.BOOL,
+					tostring(a.value ~= b.value),
+					token.loc
+				))
 
 			end
 
 		elseif token.type == TokenType.DUP then
-			local value = pop(self.stack)
+			local value = utils.pop(self.stack)
 
-			push(self.stack, value)
-			push(self.stack, value)
+			utils.push(self.stack, value)
+			utils.push(self.stack, value)
 			ip = ip + 1
 
 		elseif token.type == TokenType.IF then
-			local cond = pop(self.stack)
+			local token_cond = utils.pop(self.stack)
 
-			if not cond then
+			if token_cond == nil then
+				Error.show(
+					"Expected a 'boolean' type or 'nil'.",
+					token,
+					self.lines
+				)
+				return
+			end
+
+			if token_cond.type ~= TokenType.BOOL and
+			   token_cond.type ~= TokenType.NIL
+			then
+				Error.show(
+					("Expected to validate a 'boolean' type or 'nil', not '%s'."):format(token_cond.type:lower()),
+					token,
+					self.lines
+				)
+			end
+
+			if token_cond.value == "nil" or
+			   token_cond.value == "false"
+			then
 				ip = token.jump_ip
 			else
 				ip = ip + 1
@@ -373,9 +471,30 @@ function Parser:parse()
 			ip = token.jump_ip
 
 		elseif token.type == TokenType.DO then
-			local cond = pop(self.stack)
+			local token_cond = utils.pop(self.stack)
 
-			if not cond then
+			if token_cond == nil then
+				Error.show(
+					"Expected a 'boolean' type or 'nil'.",
+					token,
+					self.lines
+				)
+				return
+			end
+
+			if token_cond.type ~= TokenType.BOOL and
+			   token_cond.type ~= TokenType.NIL
+			then
+				Error.show(
+					("Expected to validate a 'boolean' type or 'nil', not '%s'."):format(token_cond.type:lower()),
+					token,
+					self.lines
+				)
+			end
+
+			if token_cond.value == "nil" or
+			   token_cond.value == "false"
+			then
 				ip = token.jump_ip
 			else
 				ip = ip + 1
@@ -385,48 +504,53 @@ function Parser:parse()
 			ip = ip + 1
 
 		elseif token.type == TokenType.END then
-			local returnTo = pop(returns)
+			local returnToIp = utils.pop(returns)
 
-			if returnTo ~= nil then
-				ip = returnTo
+			if returnToIp ~= nil then
+				ip = returnToIp
 
 			elseif token.jump_ip ~= nil then
 				ip = token.jump_ip
 
 			else
-				-- TODO: Maybe this is a error
 				ip = ip + 1
 			end
 
 		elseif token.type == TokenType.OVER then
-			local a = pop(self.stack)
-			local b = pop(self.stack)
+			local a = utils.pop(self.stack)
+			local b = utils.pop(self.stack)
 
-			push(self.stack, b)
-			push(self.stack, a)
-			push(self.stack, b)
+			utils.push(self.stack, b)
+			utils.push(self.stack, a)
+			utils.push(self.stack, b)
 			ip = ip + 1
 
 		elseif token.type == TokenType.SWAP then
-			local a = pop(self.stack)
-			local b = pop(self.stack)
+			local a = utils.pop(self.stack)
+			local b = utils.pop(self.stack)
 
-			push(self.stack, a)
-			push(self.stack, b)
+			utils.push(self.stack, a)
+			utils.push(self.stack, b)
 			ip = ip + 1
 
 		elseif token.type == TokenType.DROP then
-			pop(self.stack)
+			utils.pop(self.stack)
 			ip = ip + 1
 
 		elseif token.type == TokenType.DUMPSTACK then
-			local stack = table.concat(self.stack, ", ")
-			io.write("[STACK]: ["..stack.."]\n")
+			io.write("[STACK]:\n")
+
+			for idx, token_ in ipairs(self.stack) do
+				io.write("    ", token_.type, ": ", token_.value, " (", type(token_.value), ")")
+
+				io.write("\n")
+			end
+
 			ip = ip + 1
 
 		elseif token.type == TokenType.SHOW then
-			local value = pop(self.stack)
-			io.write(tostring(value), '\n')
+			local token_ = utils.pop(self.stack)
+			io.write(tostring(token_.value), '\n')
 
 			ip = ip + 1
 
@@ -434,18 +558,79 @@ function Parser:parse()
 			ip = token.jump_ip
 
 		elseif token.type == TokenType.IDENTIFIER then
-			local ipToJump = self.defines[token.value]
+			-- Verify in self.defines
+			if self.defines[token.value] ~= nil then
+				utils.push(returns, ip+1)
+				ip = self.defines[token.value]
 
-			if ipToJump == nil then
+			-- Verify in self.env
+			elseif self.env[token.value] ~= nil then
+				local nextToken = self.program[ip+1]
+
+				if nextToken == nil or nextToken.type ~= TokenType.ACCESS then
+					Error.show(
+						("Expected method access for '%s'."):format(token.value),
+						token,
+						self.lines
+					)
+				end
+
+				ip = ip + 1
+			else
 				Error.show(
 					("Undefined identifier '%s'."):format(token.value),
 					token,
 					self.lines
 				)
+
 			end
 
-			push(returns, ip+1)
-			ip = ipToJump
+		elseif token.type == TokenType.ACCESS then
+			-- module.method(value)
+			local module = nil
+			local method = nil
+
+			-- previousToken = module
+			-- nextToken = method
+			local previousToken = self.program[ip-1]
+			local nextToken = self.program[ip+1]
+
+			-- module must exists and must be a identifier that exists in self.env
+			if previousToken == nil or previousToken.type ~= TokenType.IDENTIFIER then
+				Error.show(
+					"Expected module to access method.",
+					token,
+					self.lines
+				)
+				return
+			end
+
+			-- method must exists and must be a identifier
+			if nextToken == nil or nextToken.type ~= TokenType.IDENTIFIER then
+				Error.show(
+					"Expected method to access.",
+					token,
+					self.lines
+				)
+				return
+			end
+
+			module = self.env[previousToken.value]
+			method = nextToken.value
+
+			-- Verify if module has method
+			if module[method] == nil then
+				Error.show(
+					("Undefined method '%s' for '%s'."):format(method, previousToken.value),
+					token,
+					self.lines
+				)
+			end
+
+			module[method](self.stack, nextToken, self.lines)
+
+			-- Jump method
+			ip = ip + 2
 
 		else
 			Error.show(("Not implemented: '%s'."):format(token.type), token, self.lines)
