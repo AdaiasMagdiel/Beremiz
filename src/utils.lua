b3d8c788ac71f31d6b773975bd418418dc6f0bb8
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-06-16 22:50:01 -0300

    feat: Add readFile utility function
    This commit introduces a new utility function called `readFile` to the `utils` module. This function reads a file at the given path and returns its contents as a string.
    
    The function first tries to open the file in the user's local folder. If the file is not found, it returns nil.

--- src/utils.lua
+++ src/utils.lua
@@ -8,4 +8,18 @@ function utils.pop(stack)
 	return table.remove(stack, #stack)
 end
 
+function utils.readFile(filepath)
+	-- First: Verify in user local folder
+	local fp = io.open(filepath, "r")
+
+	if fp == nil then
+		return nil
+	end
+
+	local content = fp:read("*a")
+	fp:close()
+
+	return content
+end
+
 return utils
