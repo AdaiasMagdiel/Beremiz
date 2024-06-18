b59472d410b516a7a93d74857e56338b18fa9827
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-06-18 00:05:26 -0300

    feat: add `table.length` method
    This commit adds a new method to the `table` module called `table.length`. It allows you to determine the length of a table, which can be useful for iterating over the table or performing other operations.
    
    - Adds a new method `table.length` to the `table` module.
    - This method takes a table as input.
    - It returns a number representing the length of the table.
    
    The `table.length` method is implemented in a way that is both efficient and safe. It checks for the type of the input argument to ensure that it is a table. If the input is not a table, it raises an error.
    
    This change is compatible with existing code, and does not introduce any breaking changes.

---
+++ src/modules/table.lua
@@ -0,0 +1,61 @@
+local table_methods = {}
+
+local Error = require("src.error")
+local utils = require("src.utils")
+local pprint = require("lib.pprint")
+
+local Tokens = require("src.token")
+local Token = Tokens.Token
+local TokenType = Tokens.TokenType
+
+local ErrorInfo = {
+	stack = {},
+	lines = {},
+	method_token = {}
+}
+
+local function validateCount(count)
+	if #ErrorInfo.stack < count then
+		Error.show(
+			("Expected %d arguments for 'table.%s', but only found %d arguments on the stack."):
+			format(count, ErrorInfo.method_token.value, #ErrorInfo.stack),
+			ErrorInfo.method_token,
+			ErrorInfo.lines
+		)
+	end
+end
+
+local function validateType(token, type_)
+	type_ = type_ or TokenType.TABLE
+
+	if token.type ~= type_ then
+		Error.show(
+			("Expected a table for 'table.%s' but encountered '%s'."):
+			format(ErrorInfo.method_token.value, token.type:lower()),
+			token,
+			ErrorInfo.lines
+		)
+	end
+end
+
+function table_methods.length(stack, method_token, lines)
+	ErrorInfo.stack = stack
+	ErrorInfo.lines = lines
+	ErrorInfo.method_token = method_token
+
+	validateCount(1)
+	local token = utils.pop(ErrorInfo.stack)
+	validateType(token)
+
+	utils.push(stack, Token.new(
+		TokenType.NUMBER,
+		#token.value,
+		token.loc
+	))
+end
+
+function table_methods.pop(stack, method_token, lines)
+	
+end
+
+return table_methods
